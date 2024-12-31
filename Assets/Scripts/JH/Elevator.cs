d4909633249e28f3762b34dfa8926e86fc5d1365
DESKTOP-G7NS9V3\user <wpghks1998@naver.com>
2024-12-31 20:09:03 +0900

    Elevator 오브젝트 클릭 기능 추가확인, JHScenes1 수정

--- Assets/Scripts/JH/Elevator.cs
+++ Assets/Scripts/JH/Elevator.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
 {
+    [SerializeField] private List<GameObject>[] elevatorDoor;
     public void MyFirstVRFunction()
     {
         Debug.Log("Hello, World!");
