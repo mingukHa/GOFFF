8fd9ed2c891cbdae7f191bbeef55fb1e3216dc14
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-02 15:27:45 +0900

    무전기 : 컨트롤러로 선택하면 알아서 위치에 장착되게 만듬 (버튼을 눌러야 작동되게 되있는데, 아직 어떤 버튼인지 정하지 않음) 밸브 : 현재 크놉으로 조정중

--- Assets/Scripts/MS/JoinEchoChannel.cs
+++ Assets/Scripts/MS/JoinEchoChannel.cs
@@ -12,6 +12,13 @@ public class JoinEchoChannel : MonoBehaviour
 
     public static JoinEchoChannel Instance { get; private set; }
 
+    private string channelName = "Channel";
+
+    private bool isMuted = true;
+
+    [SerializeField]
+    private WalkieAttach WalkieAttach = null;
+
     //private VivoxParticipant Participant;
 
     //private bool isMuted = true;
@@ -32,7 +39,10 @@ public class JoinEchoChannel : MonoBehaviour
 
         await LoginAsync();
 
-        OnLoginEndEvent?.Invoke();
+        await JoinVoiceChannel(channelName);
+
+        VivoxService.Instance.MuteOutputDevice();
+
 
     }
 
@@ -46,12 +56,36 @@ public class JoinEchoChannel : MonoBehaviour
 
     }
 
-    public async void JoinVoiceChannel(string channelName)
+    public async Task JoinVoiceChannel(string channelName)
     {
 
         //음성채팅 채널에 접속
         await VivoxService.Instance.JoinGroupChannelAsync(channelName, ChatCapability.AudioOnly);
         //await VivoxService.Instance.JoinEchoChannelAsync(channelName, ChatCapability.AudioOnly);
+    }
+
+    private void Update()
+    {
+        if (!WalkieAttach.IsSelected) return;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // A 키를 누르고 있을 때만 언뮤트
+            if (isMuted)
+            {
+                VivoxService.Instance.UnmuteOutputDevice();
+                isMuted = false; // 언뮤트 상태로 변경
+            }
+        }
+        else
+        {
+            // A 키를 떼면 뮤트 상태로 되돌리기
+            if (!isMuted)
+            {
+                VivoxService.Instance.MuteOutputDevice();
+                isMuted = true; // 뮤트 상태로 변경
+            }
+        }
     }
 
     //private void Update()
---
+++ Assets/Scripts/MS/Walkie Attach.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class WalkieAttach : MonoBehaviour
+{
+    [SerializeField]
+    private Transform attachPoint = null;
+    private bool isSelected = false;
+
+    public bool IsSelected { get { return isSelected; } }
+
+    private PhotonView photonView;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    public void OnSelectWalkie()
+    {
+        if (!photonView.IsMine) return; // 소유주만 이벤트 처리
+
+        Debug.Log("레이 캐스트가 됨");
+
+        // 로컬에서 위치 이동 처리
+        transform.SetParent(attachPoint, false);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        isSelected = true;
+
+        // 다른 클라이언트들에게 위치와 회전 전송
+        photonView.RPC(nameof(RPC_UpdateWalkiePosition), RpcTarget.Others, attachPoint.name);
+    }
+
+    [PunRPC]
+    private void RPC_UpdateWalkiePosition(string attachPointName)
+    {
+        // attachPoint를 찾음 (다른 클라이언트도 동일한 Transform을 가져야 함)
+        Transform newAttachPoint = GameObject.Find(attachPointName)?.transform;
+
+        if (newAttachPoint != null)
+        {
+            transform.SetParent(newAttachPoint, false);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+}
