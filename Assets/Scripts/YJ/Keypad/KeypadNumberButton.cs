3780129aacebcb90c68244be992c24dbb9694a9c
Jin <voltagevault@gmail.com>
2025-01-03 17:46:20 +0900

    *** 비밀번호 퍼즐 완성(문 개폐 동작은 다음주에) ***

--- Assets/Scripts/YJ/Keypad/KeypadNumberButton.cs
+++ Assets/Scripts/YJ/Keypad/KeypadNumberButton.cs
@@ -19,7 +19,6 @@ public class KeypadNumberButton : MonoBehaviour
 
         // 숫자 버튼을 눌렀을 때 동작
         GameObject displayedNumber = Instantiate(numberPrefab, screenDisplayPoint);
-        displayedNumber.transform.localScale = new Vector3(2, 2, 1); // x, y축으로 2배 크기 조정
         keypadManager.AddNumber(number);
     }
 }
