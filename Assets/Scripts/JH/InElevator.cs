c7fe2a4807cddebee5ab776e1a336d9e609066cb
DESKTOP-G7NS9V3\user <wpghks1998@naver.com>
2025-01-03 11:23:41 +0900

    엘리베이터 스크립트 2개 건들고있음, JHScene1, 2 건듬

--- Assets/Scripts/JH/InElevator.cs
+++ Assets/Scripts/JH/InElevator.cs
@@ -5,48 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class InElevator : MonoBehaviour
 {
-    [SerializeField] private Elevator elevator;  //Elevator 스크립트를 가지고있는 elevator변수
-    private bool isPushed = false;  //버튼 누른지 체크
+    [SerializeField] private List<Transform> elevatorDoors; //엘리베이터 4개
+    public float closeDuration = 2f; //문 열리는 시간
+    private Vector3 closedScale = new Vector3(1, 1, 1); //닫힌 상태의 Scale
+    private Vector3 openScale = new Vector3(0, 1, 1);   //열린 상태의 Scale
 
-    [SerializeField] private List<Transform> elevatorDoors; //엘리베이터 문들
-    public float closeDuration = 2f; // 문 닫히는 시간
-    private Vector3 closedScale = new Vector3(0, 0, 0); // 닫힌 상태의 Scale
-    private Vector3 openScale = new Vector3(1, 0, 0);   // 열린 상태의 Scale
-    private bool isClosing = false;
-
-    [SerializeField] private InElevatorManager elevatorManager;
-
-    private void CloseDoors()
+    public void CloseDoors()
     {
-        if(!isClosing)
-        {
-            StartCoroutine(CloseDoorsCoroutine());
-            elevatorManager.CloseDoorsDetected();
-        }
+        StartCoroutine(CloseDoorsCoroutine());
     }
 
-    private IEnumerator CloseDoorsCoroutine()
+    public IEnumerator CloseDoorsCoroutine()
     {
-        isClosing = true;
         float elapsedTime = 0f;
 
         while (elapsedTime < closeDuration)
         {
+            float t = elapsedTime / closeDuration;   //보간 비율 0 ~ 1 사이 값 계산
             for (int i = 0; i < elevatorDoors.Count; i++)
             {
-                elevatorDoors[i].localScale = 
-                    Vector3.Lerp(openScale, closedScale, elapsedTime / closeDuration);
+                elevatorDoors[i].localScale = Vector3.Lerp(openScale, closedScale, t);
             }
             elapsedTime += Time.deltaTime;
-            yield return null; // 다음 프레임까지 대기
+            yield return null;  //다음 프레임까지 대기
         }
 
         for (int i = 0; i < elevatorDoors.Count; i++)
         {
             elevatorDoors[i].localScale = closedScale;
         }
-
-        SceneManager.LoadScene("JHScenes2");
-        isClosing = false;
     }
 }
