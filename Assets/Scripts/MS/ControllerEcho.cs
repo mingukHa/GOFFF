fea2efadaae22fe79eabfae1a83f181bf4e6cf59
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-07 15:54:29 +0900

    컨트롤러 에코 새로 생성함

---
+++ Assets/Scripts/MS/ControllerEcho.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class ControllerEcho : MonoBehaviourPun
+{
+    private bool isBPressed = false;
+    private bool wasBPressed = false; // 버튼이 마지막으로 눌린 상태 추적
+    SimpleSonarShader_Parent par = null;
+
+    private void Start()
+    {
+        par = GetComponent<SimpleSonarShader_Parent>();
+    }
+    private void Update()
+    {
+        // 오른손 컨트롤러의 입력 상태 가져오기
+        InputDevice rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        // Secondary Button (B 버튼) 상태 읽기
+        if (rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out isBPressed) && isBPressed)
+        {
+            if (!wasBPressed)
+            {
+                // B 버튼이 눌렸을 때 동작
+                RaycastHit hit;
+                Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    par.StartSonarRing(hit.point, 1.4f, 0);
+                    Debug.Log($"{hit.point}입니다");
+                }
+                wasBPressed = true;
+            }
+        }
+        else
+            wasBPressed = false;
+    }
+}
--- Assets/Shader/MS/SimpleSonarShader_ExampleCollision.cs
+++ Assets/Shader/MS/SimpleSonarShader_ExampleCollision.cs
@@ -9,8 +9,7 @@ using Photon.Pun;
 public class SimpleSonarShader_ExampleCollision : MonoBehaviourPun
 {
     SimpleSonarShader_Parent par = null;
-    private bool isBPressed = false;
-    private bool wasBPressed = false; // 버튼이 마지막으로 눌린 상태 추적
+
 
     private void Start()
     {
@@ -30,31 +29,7 @@ public class SimpleSonarShader_ExampleCollision : MonoBehaviourPun
     //        }
     //    }
     //}
-    private void Update()
-    {
-        // 오른손 컨트롤러의 입력 상태 가져오기
-        InputDevice rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
 
-        // Secondary Button (B 버튼) 상태 읽기
-        if (rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out isBPressed) && isBPressed)
-        {
-            if (!wasBPressed)
-            {
-                // B 버튼이 눌렸을 때 동작
-                RaycastHit hit;
-                Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-
-                if (Physics.Raycast(ray, out hit))
-                {
-                    par.StartSonarRing(hit.point, 1.4f, 0);
-                    Debug.Log($"{hit.point}입니다");
-                }
-                wasBPressed = true;
-            }
-        }
-        else
-            wasBPressed= false;
-    }
 
     void OnCollisionEnter(Collision collision)
     {
