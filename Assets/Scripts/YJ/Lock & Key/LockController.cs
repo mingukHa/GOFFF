908d2b4ee9e4ee940f70f297db17528172cad64c
Jin <voltagevault@gmail.com>
2025-01-08 18:05:36 +0900

    *** 지하 3층 도플갱어방 자물쇠 추가 *** *** 집어들 수 있는 오브젝트 벽뚫기 방지 ***

--- Assets/Scripts/YJ/Lock & Key/LockController.cs
+++ Assets/Scripts/YJ/Lock & Key/LockController.cs
@@ -1,40 +1,40 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class LockController : MonoBehaviour
+public class LockController : MonoBehaviourPun
 {
     // DoorController를 참조할 필드
     public DoorController doorController;
+    private Collider lockColliders;
 
-    private Collider[] lockColliders;
-
-    void Start()
+    private void Start()
     {
-        // Lock 오브젝트의 하위 Collider를 가져옵니다
-        lockColliders = GetComponentsInChildren<Collider>();
-
-        // 각 Collider에 Trigger 이벤트 처리를 위한 설정
-        foreach (var collider in lockColliders)
-        {
-            if (!collider.isTrigger)
-            {
-                collider.isTrigger = true;
-            }
-        }
+        lockColliders = GetComponent<Collider>();
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         // 충돌한 오브젝트가 'Key' 태그를 가지고 있는지 확인
         if (other.CompareTag("Key"))
         {
-            // DoorController의 OpenDoor 메서드 호출
+            // DoorController의 OpenDoor 메서드 호출 및 동기화
             if (doorController != null)
             {
-                doorController.OpenDoor();
+                photonView.RPC("OpenDoorAndDestroy", RpcTarget.All);
             }
-
-            // Lock 오브젝트 제거
-            Destroy(gameObject);
         }
+    }
+
+    [PunRPC]
+    private void OpenDoorAndDestroy()
+    {
+        // DoorController의 OpenDoor 메서드 호출
+        if (doorController != null)
+        {
+            doorController.OpenDoor();
+        }
+
+        // Lock 오브젝트 제거
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/YJ/PowerLever/LeverController.cs
+++ Assets/Scripts/YJ/PowerLever/LeverController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 using Photon.Pun;
 
-
 public class LeverController : MonoBehaviourPun
 {
     // 핸들, 엘리베이터 오브젝트
