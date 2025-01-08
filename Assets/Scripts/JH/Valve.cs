ec687f494aa4467aa259c7ffb1f27484f3c98cbb
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-08 12:07:26 +0900

    밸브 rpc로 크놉 밸브 꺼지게 수정 그랩 밸브 키네마틱 켜지게 수정

--- Assets/Scripts/JH/Valve.cs
+++ Assets/Scripts/JH/Valve.cs
@@ -94,7 +94,7 @@ public class Valve : MonoBehaviourPun
         StartCoroutine(Delay);
 
         knobValve.SetActive(false);
-        //photonView.RPC("RPCknobValvefalse", RpcTarget.OthersBuffered);
+        photonView.RPC("RPCknobValvefalse", RpcTarget.Others);
         grabValve.transform.position = cylinderAttachPoint.position;  // 밸브의 위치를 AttachPoint 위치로 설정
         grabValve.transform.rotation = cylinderAttachPoint.rotation;  // 밸브의 회전을 AttachPoint 회전으로 설정
         Rigidbody grabValveRb = grabValve.GetComponent<Rigidbody>();
@@ -142,11 +142,6 @@ public class Valve : MonoBehaviourPun
     private void RPCValveGrab(bool grabbed)
     {
         isGrabbed = grabbed;
-        Rigidbody grabValveRD= grabValve.GetComponent<Rigidbody>();
-        if (grabValveRD != null)
-        {
-            grabValveRD.isKinematic = grabbed;
-        }
     }
 
     [PunRPC]
