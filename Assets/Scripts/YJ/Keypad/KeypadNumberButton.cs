d7eb856888506802e67c3ab6354a286d7136e1c0
Jin <voltagevault@gmail.com>
2025-01-08 14:31:12 +0900

    *** 3층 작업 중 ***

--- Assets/Scripts/YJ/Keypad/KeypadNumberButton.cs
+++ Assets/Scripts/YJ/Keypad/KeypadNumberButton.cs
@@ -6,16 +6,18 @@ public class KeypadNumberButton : MonoBehaviour
     public int number; // 이 버튼이 나타내는 숫자
     public Transform screenDisplayPoint; // 숫자가 표시될 화면의 부모 Transform
     public GameObject numberPrefab; // 표시할 숫자 프리팹
-
-    private KeypadManager keypadManager;
+    private SoundManager soundManager; // 사운드 매니저
+    private KeypadManager keypadManager; // 키패드 매니저
 
     private void Start()
     {
         keypadManager = FindObjectOfType<KeypadManager>();
+        soundManager = FindObjectOfType<SoundManager>();
     }
 
     public void OnPressed()
     {
+        SoundManager.instance.SFXPlay("Button2_SFX");
         Debug.Log(number + "번 버튼을 눌렀습니다.");
 
         // RPC를 통해 숫자 추가 동작을 동기화
