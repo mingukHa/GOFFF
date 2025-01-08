fb52a7ac49106ea8e78fc0d2f762fd7acf9feba1
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-08 16:02:32 +0900

    no message

--- Assets/Scripts/MG/wil/Waitscene.cs
+++ Assets/Scripts/MG/wil/Waitscene.cs
@@ -101,14 +101,6 @@ public class Waitscene : MonoBehaviourPunCallbacks
     public void OnButtonPressed()
     {
         photonView.RPC("PlayerReady", RpcTarget.AllBuffered); // 모든 클라이언트에 플레이어 준비 상태 전달
-        Debug.Log("버튼이 눌렸습니다");
-    }
-
-    [PunRPC]
-    public void PlayerReady()
-    {
-        readyPlayerCount++;
-
         Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}");
 
         // 모든 플레이어가 준비되었을 경우 다음 씬으로 전환
@@ -117,6 +109,13 @@ public class Waitscene : MonoBehaviourPunCallbacks
             Debug.Log("2명 준비 완료! 다음 씬으로 이동합니다.");
             PhotonNetwork.LoadLevel("MainScenes"); // 전환할 씬 이름으로 변경
         }
+        Debug.Log("버튼이 눌렸습니다");
+    }
+
+    [PunRPC]
+    public void PlayerReady()
+    {
+        readyPlayerCount++;
     }
 
     public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
