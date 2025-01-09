85184cd578c7c422db331e5ce0bbf8e8ff7f3e3e
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 15:53:09 +0900

    no message

--- Assets/Scripts/MG/Floor/ValveTrigger.cs
+++ Assets/Scripts/MG/Floor/ValveTrigger.cs
@@ -6,6 +6,7 @@ public class ValveTrigger : MonoBehaviourPun
     public GameObject cage;
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("트리거 활성화");
         PhotonNetwork.Destroy(cage);
     }
 }
