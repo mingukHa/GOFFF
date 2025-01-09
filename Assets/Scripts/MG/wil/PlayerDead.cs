205bfa4c1aa411b2e811b5d0e1acfe935e4dbf1b
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 12:09:25 +0900

    no message

--- Assets/Scripts/MG/wil/PlayerDead.cs
+++ Assets/Scripts/MG/wil/PlayerDead.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 public class PlayerDead : MonoBehaviourPun
 {
     private bool isDead = false;
+    public delegate void CollisionEvent(bool isDead);
+    public static event CollisionEvent PlayerDie;
     //string SceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Monster"))
             isDead = true;
         Invoke("ReStart", 5f);
+    }
+    private void FixedUpdate()
+    {
+        hight();
+    }
+    private void hight()
+    {
+        if (transform.position.y >= -10f)
+        {
+            isDead = true;
+            Invoke("ReStart", 1f);
+        }
     }
 
     [PunRPC]
