af06377fe2de8175edec819b07a4775c66c59df8
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 19:27:44 +0900

    no message

--- Assets/Scripts/MG/wil/PlayerDead.cs
+++ Assets/Scripts/MG/wil/PlayerDead.cs
@@ -1,18 +1,33 @@
-using Photon.Pun;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerDead : MonoBehaviourPun
 {
-      
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Monster"))           
-        Invoke("ReStart", 5f);
+        if (collision.gameObject.CompareTag("Monster"))
+        {
+            Invoke(nameof(SendRestartRPC), 2f); // 2초 뒤 RPC 호출
+        }
     }
+
+    private void SendRestartRPC()
+    {
+        // 모든 클라이언트에 씬 리로드 요청
+        photonView.RPC("ReStart", RpcTarget.All);
+    }
+
     [PunRPC]
     private void ReStart()
     {
-        string SceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-        PhotonNetwork.LoadLevel(SceneName);        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            string SceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+            PhotonNetwork.LoadLevel(SceneName);
+        }
+        else
+        {
+            Debug.Log("씬 로드는 마스터 클라이언트에서 처리됩니다.");
+        }
     }
 }
