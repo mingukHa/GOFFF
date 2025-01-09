59fbc11794a071efc05773d3d87cad5c473bb028
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 13:12:14 +0900

    no message

--- Assets/Scripts/MG/wil/Waitscene.cs
+++ Assets/Scripts/MG/wil/Waitscene.cs
@@ -117,20 +117,23 @@ public class Waitscene : MonoBehaviourPunCallbacks
     // 버튼 클릭 시 호출되는 메서드
     public void OnButtonPressed()
     {
-        photonView.RPC("PlayerReady", RpcTarget.AllBuffered); // 모든 클라이언트에 플레이어 준비 상태 전달
-        Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}");
-        //if(isMasterClient)
-        // 모든 플레이어가 준비되었을 경우 다음 씬으로 전환
-        if (readyPlayerCount == 2)
+        if (PhotonNetwork.IsMasterClient)
         {
-            
-            Debug.Log("2명 준비 완료! 다음 씬으로 이동합니다.");
-            PhotonNetwork.LoadLevel("MainScenes"); // 전환할 씬 이름으로 변경
-            
-        }
-        else
-        {
-            return;
+            photonView.RPC("PlayerReady", RpcTarget.AllBuffered); // 모든 클라이언트에 플레이어 준비 상태 전달
+            Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}");
+            //if(isMasterClient)
+            // 모든 플레이어가 준비되었을 경우 다음 씬으로 전환
+            if (readyPlayerCount == 2)
+            {
+
+                Debug.Log("2명 준비 완료! 다음 씬으로 이동합니다.");
+                PhotonNetwork.LoadLevel("MainScenes"); // 전환할 씬 이름으로 변경
+
+            }
+            else
+            {
+                return;
+            }
         }
         Debug.Log("버튼이 눌렸습니다");
     }
