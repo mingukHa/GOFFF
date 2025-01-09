fd33bf97e1da1761c370d0382962fd87f9df0c39
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 10:22:22 +0900

    no message

--- Assets/Scripts/MG/wil/Waitscene.cs
+++ Assets/Scripts/MG/wil/Waitscene.cs
@@ -121,8 +121,8 @@ public class Waitscene : MonoBehaviourPunCallbacks
         Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}");
 
         // 모든 플레이어가 준비되었을 경우 씬 전환
-        if (readyPlayerCount >= playerStart)
-        {
+        if (playerStart >= readyPlayerCount)
+        {   
             Debug.Log("2명 준비 완료! 다음 씬으로 이동합니다.");
             PhotonNetwork.LoadLevel("MainScenes"); // 전환할 씬 이름으로 변경
         }
