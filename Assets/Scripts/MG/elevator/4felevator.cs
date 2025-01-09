10b7ef676b877a37c8fed370440176bc8fac8ac7
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-09 17:42:11 +0900

    no message

--- Assets/Scripts/MG/elevator/4felevator.cs
+++ Assets/Scripts/MG/elevator/4felevator.cs
@@ -12,8 +12,8 @@ public class f4elevators : MonoBehaviourPunCallbacks
 
     public void OnButtonPressed()
     {
-        //if (PhotonNetwork.IsMasterClient)
-       // {
+        if (PhotonNetwork.IsMasterClient)
+        {
             photonView.RPC("PlayerReady", RpcTarget.AllBuffered); // 모든 클라이언트에 플레이어 준비 상태 전달
             Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}");
 
@@ -29,7 +29,7 @@ public class f4elevators : MonoBehaviourPunCallbacks
             {
                 return;
             }
-      //  }
+        }
         Debug.Log("버튼이 눌렸습니다");
     }
     //JHScenes2 , JHScenes3 , MainScenes 
