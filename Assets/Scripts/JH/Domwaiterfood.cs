738e4aaccd583aafe7af6a748c1574e0493281a5
DESKTOP-G7NS9V3\user <wpghks1998@naver.com>
2025-01-10 11:58:16 +0900

    JHScenes1에서 덤웨이터 기능 제작하고 테스트 하려고하는 중

--- Assets/Scripts/JH/Domwaiterfood.cs
+++ Assets/Scripts/JH/Domwaiterfood.cs
@@ -1,13 +1,15 @@
-using System.Collections;
 using UnityEngine;
+using Photon.Pun;
 
-public class Domwaiterfood : MonoBehaviour
+public class Domwaiterfood : MonoBehaviourPun
 {
     [SerializeField] private DomwaiterOven triggerZone0;   // 주황색 박스 [0]
     [SerializeField] private Transform targetPosition;  // 주황색 박스 [1]
 
     public void OnButtonPressed()
     {
+        /*  멀티가 아닌 상황에서 로컬 로직
+        
         // 주황색 박스[0]dptj 충돌된 모든 오브젝트 가져오기
         var objectsToMove = triggerZone0.GetObjectsInZone();
 
@@ -15,6 +17,14 @@ public class Domwaiterfood : MonoBehaviour
         foreach (GameObject obj in objectsToMove)
         {
             obj.transform.position = targetPosition.position;
+        }
+
+        */
+
+        if (photonView.IsMine) // 이 버튼을 누른 플레이어만 아이템 전송 요청을 보냄
+        {
+            // 주황색 박스[0]에서 아이템을 전송
+            triggerZone0.SendItemsToTarget(targetPosition);
         }
     }
 }
