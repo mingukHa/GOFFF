33238e4f96758cb50fdefec14ed7289c1c646656
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-13 10:30:46 +0900

    오전 병합

--- Assets/Scripts/MG/elevator/4felevator.cs
+++ Assets/Scripts/MG/elevator/4felevator.cs
@@ -12,24 +12,24 @@ public class f4elevators : MonoBehaviourPunCallbacks
 
     public void OnButtonPressed()
     {
-        if (PhotonNetwork.IsMasterClient)
-        {
+        
             photonView.RPC("PlayerReady1", RpcTarget.AllBuffered); // 모든 클라이언트에 플레이어 준비 상태 전달
             Debug.Log($"현재 준비된 플레이어 수: {readyPlayerCount}/{PhotonNetwork.CurrentRoom.PlayerCount}"); //준비 인원 / 현재 방 참여 인원
 
             // 모든 플레이어가 준비되었을 경우 다음 씬으로 전환
             if (readyPlayerCount == 2)
             {
-
+                if (PhotonNetwork.IsMasterClient)
+                {
                 Debug.Log("2명 준비 완료! 다음 씬으로 이동합니다.");
                 PhotonNetwork.LoadLevel(Scene); // 전환할 씬 이름으로 변경
-
+                }
             }
             else
             {
                 return;
             }
-        }
+        
         Debug.Log("버튼이 눌렸습니다");
     }
     //JHScenes2 , JHScenes3 , MainScenes 
