a91313c5ee057c3d83e9a16dc30f57f6ec218aa6
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-13 20:49:41 +0900

    no message

--- Assets/Scripts/MG/wil/IsMain.cs
+++ Assets/Scripts/MG/wil/IsMain.cs
@@ -3,66 +3,114 @@ using Photon.Pun;
 
 public class PlayerControllerManager : MonoBehaviourPun
 {
-    private GameObject CameraOffset;
-    private GameObject handOffset;
+    private GameObject _cameraOffset;
+    private GameObject _handOffset;
 
+    // Lazy Initialization
+    private GameObject CameraOffset
+    {
+        get
+        {
+            if (_cameraOffset == null)
+            {
+                _cameraOffset = transform.Find("Cameras")?.gameObject;
+                if (_cameraOffset == null)
+                {
+                    Debug.LogError("CameraOffset을 찾을 수 없습니다!");
+                }
+            }
+            return _cameraOffset;
+        }
+    }
+
+    private GameObject HandOffset
+    {
+        get
+        {
+            if (_handOffset == null)
+            {
+                _handOffset = transform.Find("handOffset")?.gameObject;
+                if (_handOffset == null)
+                {
+                    Debug.LogError("HandOffset을 찾을 수 없습니다!");
+                }
+            }
+            return _handOffset;
+        }
+    }
 
     private void Awake()
     {
-        CameraOffset = transform.Find("Cameras")?.gameObject;
-        handOffset = transform.Find("handOffset")?.gameObject;
-
+        // 소유 여부에 따라 카메라와 컨트롤러 초기화
         if (!photonView.IsMine)
         {
             DisableControllers();
-            //photonView.RequestOwnership();
-            Disablehand();
+            DisableHand();
         }
         else
         {
             EnableControllers();
-            Enablehand();
+            EnableHand();
         }
     }
+
     private void Start()
     {
+        // 상대방의 카메라 및 렌더러/콜라이더 비활성화
         if (!photonView.IsMine)
         {
-            // 다른 플레이어의 컨트롤러는 렌더링 및 상호작용 비활성화
-            foreach (var renderer in GetComponentsInChildren<Renderer>())
-            {
-                renderer.enabled = false;
-            }
+            DisableRenderersAndColliders();
+        }
+    }
 
-            foreach (var collider in GetComponentsInChildren<Collider>())
-            {
-                collider.enabled = false;
-            }
+    private void DisableRenderersAndColliders()
+    {
+        // 렌더러 비활성화
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = false;
+        }
+
+        // 콜라이더 비활성화
+        foreach (var collider in GetComponentsInChildren<Collider>())
+        {
+            collider.enabled = false;
         }
     }
 
     private void DisableControllers()
     {
-        if (CameraOffset != null) CameraOffset.SetActive(false);
-
-        Debug.Log("비활성화 했슈");
+        if (CameraOffset != null)
+        {
+            CameraOffset.SetActive(false);
+            Debug.Log("상대방 카메라 비활성화 완료");
+        }
     }
 
     private void EnableControllers()
     {
-        if (CameraOffset != null) CameraOffset.SetActive(true);
-        Debug.Log("활성화 했슈");
-    }
-    private void Disablehand()
-    {
-        if (CameraOffset != null) CameraOffset.SetActive(false);
-
-        Debug.Log("비활성화 했슈");
+        if (CameraOffset != null)
+        {
+            CameraOffset.SetActive(true);
+            Debug.Log("내 카메라 활성화 완료");
+        }
     }
 
-    private void Enablehand()
+    private void DisableHand()
     {
-        if (CameraOffset != null) CameraOffset.SetActive(true);
-        Debug.Log("활성화 했슈");
+        if (HandOffset != null)
+        {
+            HandOffset.SetActive(false);
+            Debug.Log("상대방 손 비활성화 완료");
+        }
+    }
+
+    private void EnableHand()
+    {
+        if (HandOffset != null)
+        {
+            HandOffset.SetActive(true);
+            Debug.Log("내 손 활성화 완료");
+        }
     }
 }
