936d42c6e314452802d173ce771089d65d505d6c
Jin <voltagevault@gmail.com>
2025-01-13 16:24:10 +0900

    지하2층 중간 완성, PlayerHolder 설정 조정, 스크립트 수정

--- Assets/Scripts/YJ/PlayerController.cs
+++ Assets/Scripts/YJ/PlayerController.cs
@@ -88,7 +88,7 @@ public class PlayerController : MonoBehaviourPun
         isRGripTriggerPressed = context.ReadValue<float>() > 0.5f;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // 이전 프레임과 비교해 이동 거리 계산
         Vector3 leftDelta = leftVelocity - lastLeftPosition;
@@ -134,15 +134,14 @@ public class PlayerController : MonoBehaviourPun
         // 회전 처리
         HandleRotation();
 
-        Vector3 finalMovement = movement * Time.deltaTime;
+        Vector3 finalMovement = movement * Time.fixedDeltaTime;
 
         // 최대 속도 제한
         if (finalMovement.magnitude > maxSpeed)
         {
             finalMovement = finalMovement.normalized * maxSpeed;
         }
-
-
+        
         // 카메라 리그 이동
         playerHolder.position += finalMovement;
 
@@ -229,7 +228,7 @@ public class PlayerController : MonoBehaviourPun
         float wheelRotation = movementMagnitude * wheelRotationMultiplier * direction;
 
         // 디버깅
-        Debug.Log($"로컬 이동 방향: {localMovement}, 최종 이동 방향: {movementMagnitude * direction}");
+        //Debug.Log($"로컬 이동 방향: {localMovement}, 최종 이동 방향: {movementMagnitude * direction}");
 
         // 바퀴 회전 처리
         if (isLGripTriggerPressed && !isRGripTriggerPressed) // 우회전
