5d9d8423280786424f53fd47ecd10a814b537cf7
MINGUK\h1865 <gkalsrnr12@naver.com>
2025-01-13 17:35:39 +0900

    몬스터 링

--- Assets/Material/MG/MonsterSona.cs
+++ Assets/Material/MG/MonsterSona.cs
@@ -6,7 +6,7 @@ public class MonsterSona : MonoBehaviour
     private SimpleSonarShader_Parent par; // Inspector에서 직접 설정
     private Animator animator;
     //private bool monster = true;
-    private enum MonsterState { Idle, Walking, LookingAround, Attack, Returning, Detect }
+    private enum MonsterState { Idle, Walking, Detect }
     private MonsterState currentState = MonsterState.Idle;
     private void Awake()
     {
@@ -16,43 +16,37 @@ public class MonsterSona : MonoBehaviour
     {
         // 코루틴 시작
         par = FindFirstObjectByType<SimpleSonarShader_Parent>();
-        
+        StartCoroutine("MonstersonaIdle");
 
     }
-    private void Update()
+    
+    private IEnumerator MonstersonaIdle()
     {
+        Debug.Log("코루틴 실행");
+        
         switch (currentState)
         {
             case MonsterState.Idle:
-                //StartCoroutine(MonstersonaIdle(1,1));
+                MonsterSonas(1.4f, 1);
+                yield return new WaitForSeconds(2f);
                 break;
             case MonsterState.Walking:
-                //StartCoroutine(MonstersonaIdle(0.5f,2));
-                break;
-            case MonsterState.LookingAround:
-                
+                MonsterSonas(1f, 2);
+                yield return new WaitForSeconds(1.3f);
                 break;
             case MonsterState.Detect:
+                MonsterSonas(0.7f, 2);
+                yield return new WaitForSeconds(0.7f);
+                break;
 
-                //StartCoroutine(MonstersonaIdle(0.5f, 3));
-                break;
-            case MonsterState.Attack:
-                
-                break;
         }
     }
-    private IEnumerator MonstersonaIdle(float time, int powar)
+   private void MonsterSonas(float intensity, int type)
     {
-        Debug.Log("코루틴 실행");
         while (true)
         {
             Vector4 position = new Vector4(transform.position.x, transform.position.y, transform.position.z, 0);
-            if (par) par.StartSonarRing(position, time, powar);
-            Debug.Log("Sonar Ring 실행: " + transform.position);
-
-            // 1초 대기
-            yield return new WaitForSeconds(1f);
+            if (par) par.StartSonarRing(position, intensity, type);       
         }
     }
-   
 }
