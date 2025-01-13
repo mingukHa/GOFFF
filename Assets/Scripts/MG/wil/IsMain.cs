af43c2ef2bbaa7fe981b7085e8f59f3bd1623818
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-13 18:44:03 +0900

    PlayerHolder에 태그 추가해서 끄는기능추가
    IsMain의 스크립트 수정

--- Assets/Scripts/MG/wil/IsMain.cs
+++ Assets/Scripts/MG/wil/IsMain.cs
@@ -3,66 +3,83 @@ using Photon.Pun;
 
 public class PlayerControllerManager : MonoBehaviourPun
 {
-    private GameObject CameraOffset;
-    private GameObject handOffset;
+    private GameObject[] CameraOffset;
+    private GameObject[] handOffset;
 
 
     private void Awake()
     {
-        CameraOffset = transform.Find("Cameras")?.gameObject;
-        handOffset = transform.Find("handOffset")?.gameObject;
-
-        if (!photonView.IsMine)
+        CameraOffset = GameObject.FindGameObjectsWithTag("PlayerCamera");
+        handOffset = GameObject.FindGameObjectsWithTag("PlayerController");
+        foreach(GameObject obj in CameraOffset)
         {
-            DisableControllers();
-            //photonView.RequestOwnership();
-            Disablehand();
-        }
-        else
-        {
-            EnableControllers();
-            Enablehand();
-        }
-    }
-    private void Start()
-    {
-        if (!photonView.IsMine)
-        {
-            // 다른 플레이어의 컨트롤러는 렌더링 및 상호작용 비활성화
-            foreach (var renderer in GetComponentsInChildren<Renderer>())
+            if(!obj.GetPhotonView().IsMine)
             {
-                renderer.enabled = false;
-            }
-
-            foreach (var collider in GetComponentsInChildren<Collider>())
-            {
-                collider.enabled = false;
+                obj.SetActive(false);
             }
         }
+        foreach(GameObject obj in handOffset)
+        {
+            if(!obj.GetPhotonView().IsMine)
+            {
+                obj.SetActive(false);
+            }
+        }
+        //handOffset = transform.Find("handOffset")?.gameObject;
+
+
+        //if (!photonView.IsMine)
+        //{
+        //    DisableControllers();
+        //    //photonView.RequestOwnership();
+        //    Disablehand();
+        //}
+        //else
+        //{
+        //    EnableControllers();
+        //    Enablehand();
+        //}
     }
 
-    private void DisableControllers()
-    {
-        if (CameraOffset != null) CameraOffset.SetActive(false);
+    //private void Start()
+    //{
+    //    if (!photonView.IsMine)
+    //    {
+    //        // 다른 플레이어의 컨트롤러는 렌더링 및 상호작용 비활성화
+    //        foreach (var renderer in GetComponentsInChildren<Renderer>())
+    //        {
+    //            renderer.enabled = false;
+    //        }
 
-        Debug.Log("비활성화 했슈");
-    }
+    //        foreach (var collider in GetComponentsInChildren<Collider>())
+    //        {
+    //            collider.enabled = false;
+    //        }
+    //    }
+    //}
 
-    private void EnableControllers()
-    {
-        if (CameraOffset != null) CameraOffset.SetActive(true);
-        Debug.Log("활성화 했슈");
-    }
-    private void Disablehand()
-    {
-        if (CameraOffset != null) CameraOffset.SetActive(false);
+    //private void DisableControllers()
+    //{
+    //    if (CameraOffset != null) CameraOffset.SetActive(false);
 
-        Debug.Log("비활성화 했슈");
-    }
+    //    Debug.Log("비활성화 했슈");
+    //}
 
-    private void Enablehand()
-    {
-        if (CameraOffset != null) CameraOffset.SetActive(true);
-        Debug.Log("활성화 했슈");
-    }
+    //private void EnableControllers()
+    //{
+    //    if (CameraOffset != null) CameraOffset.SetActive(true);
+    //    Debug.Log("활성화 했슈");
+    //}
+    //private void Disablehand()
+    //{
+    //    if (CameraOffset != null) CameraOffset.SetActive(false);
+
+    //    Debug.Log("비활성화 했슈");
+    //}
+
+    //private void Enablehand()
+    //{
+    //    if (CameraOffset != null) CameraOffset.SetActive(true);
+    //    Debug.Log("활성화 했슈");
+    //}
 }
