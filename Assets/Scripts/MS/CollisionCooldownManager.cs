43240e8ec6d62edddb73501ab935f6b28e3a9b47
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-15 20:35:09 +0900

    파동 늦게 나오는 버그 수정

--- Assets/Scripts/MS/CollisionCooldownManager.cs
+++ Assets/Scripts/MS/CollisionCooldownManager.cs
@@ -1,74 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Photon.Pun;
 
-public class CollisionCooldownManager : MonoBehaviour
-{
-    public static CollisionCooldownManager Instance { get; private set; }
+//public class CollisionCooldownManager : MonoBehaviourPun
+//{
+//    public static CollisionCooldownManager Instance { get; private set; }
 
-    private Dictionary<int, float> collisionCooldowns = new Dictionary<int, float>();
-    private float cooldownDuration = 1f; // 기본 쿨타임
-    private float cleanupInterval = 2f; // 기본 정리 주기
+//    private Dictionary<int, float> collisionCooldowns = new Dictionary<int, float>();
+//    private float cooldownDuration = 1f; // 기본 쿨타임
+//    private float cleanupInterval = 2f; // 기본 정리 주기
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            StartCoroutine(CleanupCooldowns());
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+//    private double sceneStartTimePhoton;
+//    private float timeSinceSceneLoadPhoton;
 
-    // 특정 충돌 ID의 쿨타임 체크 및 갱신
-    public bool IsCooldownActive(int id)
-    {
-        float currentTime = Time.time;
 
-        if (collisionCooldowns.TryGetValue(id, out float lastCollisionTime))
-        {
-            if (currentTime - lastCollisionTime < cooldownDuration)
-            {
-                return true; // 쿨타임 중이면 반환
-            }
-        }
+//    private void Awake()
+//    {
+//        if (Instance == null)
+//        {
+//            Instance = this;
+//            DontDestroyOnLoad(gameObject);
+//            StartCoroutine(CleanupCooldowns());
+//        }
+//        else
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
 
-        // 쿨다운 갱신
-        collisionCooldowns[id] = currentTime;
-        return false;
-    }
+//    private void Start()
+//    {
+//        sceneStartTimePhoton = PhotonNetwork.Time;
+//    }
 
-    // 쿨타임 설정
-    public void SetCooldownDuration(float duration)
-    {
-        cooldownDuration = Mathf.Max(0, duration); // 음수 방지
-    }
+//    // 특정 충돌 ID의 쿨타임 체크 및 갱신
+//    public bool IsCooldownActive(int id)
+//    {
+//        float currentTime = Time.time;
 
-    private IEnumerator CleanupCooldowns()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(cleanupInterval);
+//        if (collisionCooldowns.TryGetValue(id, out float lastCollisionTime))
+//        {
+//            if (currentTime - lastCollisionTime < cooldownDuration)
+//            {
+//                return true; // 쿨타임 중이면 반환
+//            }
+//        }
 
-            float currentTime = Time.time;
-            List<int> keysToRemove = new List<int>();
+//        // 쿨다운 갱신
+//        collisionCooldowns[id] = currentTime;
+//        return false;
+//    }
 
-            foreach (var cooldown in collisionCooldowns)
-            {
-                if (currentTime - cooldown.Value >= cleanupInterval)
-                {
-                    keysToRemove.Add(cooldown.Key);
-                }
-            }
+//    // 쿨타임 설정
+//    public void SetCooldownDuration(float duration)
+//    {
+//        cooldownDuration = Mathf.Max(0, duration); // 음수 방지
+//    }
 
-            foreach (var key in keysToRemove)
-            {
-                collisionCooldowns.Remove(key);
-            }
-        }
-    }
-}
+//    private IEnumerator CleanupCooldowns()
+//    {
+//        while (true)
+//        {
+//            yield return new WaitForSeconds(cleanupInterval);
+
+//            float currentTime = Time.time;
+//            List<int> keysToRemove = new List<int>();
+
+//            foreach (var cooldown in collisionCooldowns)
+//            {
+//                if (currentTime - cooldown.Value >= cleanupInterval)
+//                {
+//                    keysToRemove.Add(cooldown.Key);
+//                }
+//            }
+
+//            foreach (var key in keysToRemove)
+//            {
+//                collisionCooldowns.Remove(key);
+//            }
+//        }
+//    }
+//}
