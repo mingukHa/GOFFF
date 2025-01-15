8712f5d180ae9070da6e9bc46ce5ffa3c31bf33c
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2025-01-15 16:04:28 +0900

    씬 4번 수정

--- Assets/Scripts/YJ/SecretWallDestroyer.cs
+++ Assets/Scripts/YJ/SecretWallDestroyer.cs
@@ -2,25 +2,29 @@ using UnityEngine;
 
 public class SecretWallDestroyer : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private bool isCollision = false;
+    private float newRotationX;
+
+    private void Update()
     {
-        // 충돌한 객체에 PlayerController 스크립트가 있는지 확인
-        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
-
-        if (playerController != null)
+        if (isCollision)
         {
-            // finalMovement.magnitude 값을 가져오기 위해 PlayerController 속성 사용
-            float movementMagnitude = playerController.photonView.IsMine
-                ? playerController.GetCurrentMovementMagnitude()
-                : 0f;
-
-            Debug.Log("현재 속도: " + movementMagnitude);
-
-            // finalMovement.magnitude가 4 이상일 때 객체 파괴
-            if (movementMagnitude >= 4f)
+            newRotationX = Mathf.Lerp(0, 90f, Time.deltaTime * 5f);
+            transform.rotation = Quaternion.Euler(new Vector3(newRotationX, transform.rotation.y, transform.rotation.z));
+            if (transform.position.x < 1f)
             {
-                Destroy(gameObject); // SecretWallDestroyer 객체 파괴
+                Invoke("DestroygameObject", 2f);
             }
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            isCollision = true;
+    }
+
+    private void DestroygameObject()
+    {
+        Destroy(gameObject);
     }
 }
